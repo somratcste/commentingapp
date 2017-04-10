2f63e1db3caacb2148cfb992cd92ac3c86129ffa
somratcste <somratcste@gmail.com>
2017-04-11 02:55:22 +0530

    add controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CommentController extends Controller
+{
+
+    public function index()
+    {
+        return Response::json(Comment::get());
+    }
+
+
+    public function create()
+    {
+
+    }
+
+
+    public function store(Request $request)
+    {
+        Comment::create(array(
+            'author' => Input::get('author'),
+            'text' => Input::get('text')
+        ));
+
+        return Response::json(array('success' => true));
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        Comment::destroy($id);
+
+        return Response::json(array('success' => true));
+    }
+}
