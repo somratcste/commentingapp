d56041c25d89aaef4189f7edb3f93cd65c790fd1
somratcste <somratcste@gmail.com>
2017-04-11 11:09:23 +0530

    add all

---
+++ public/js/mainApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by somrat on 4/11/17.
+ */
+var commentApp = angular.module('commentApp', ['mainCtrl', 'commentService']);
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::group(array('prefix' =>  'api') , function() {
