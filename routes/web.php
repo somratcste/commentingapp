780870b09096966275c61c153d7cd670cc6b46ec
somratcste <somratcste@gmail.com>
2017-04-11 10:11:34 +0530

    add  angular service

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(array('prefix' =>  'api') , function() {
+    Route::resource('comments','CommentController');
+});
+
